ff5cdc3435badd46c2f06cc138bb58e4ba963847
Nathan Auckett <n.auckett@gmail.com>
2025-04-27 20:17:29 +1000

    Added YouTube

--- src/link.js
+++ src/link.js
@@ -34,7 +34,7 @@ function AddLink(title = "Link", image = "", url = "", type = LINK_TYPE.SOCIAL){
 
 //Socials
 AddLink(
-    "Blue Sky",
+    "BlueSky",
     "./src/graphics/Bluesky_Logo.svg",
     "https://bsky.app/profile/nathanauckett.bsky.social",
     LINK_TYPE.SOCIAL
@@ -46,7 +46,13 @@ AddLink(
     LINK_TYPE.SOCIAL
 );
 AddLink(
-    "X",
+    "YouTube",
+    "./src/graphics/youtube-play-sm.png",
+    "https://www.youtube.com/@NathanAuckett",
+    LINK_TYPE.SOCIAL
+);
+AddLink(
+    "X / Twitter",
     "./src/graphics/x-logo.svg",
     "https://x.com/NathanAuckett",
     LINK_TYPE.SOCIAL
@@ -54,14 +60,14 @@ AddLink(
 
 //Games
 AddLink(
-    "The Cardinal Crusade",
-    "./src/graphics/the-cardinal-crusade.jpg",
-    "https://gamejolt.com/games/thecardinalcrusade/171762",
-    LINK_TYPE.GAME
-);
-AddLink(
     "Eldritchvania",
     "./src/graphics/eldritchvania.jpg",
     "https://store.steampowered.com/app/2423710/Eldritchvania/",
+    LINK_TYPE.GAME
+);
+AddLink(
+    "The Cardinal Crusade",
+    "./src/graphics/the-cardinal-crusade.jpg",
+    "https://gamejolt.com/games/thecardinalcrusade/171762",
     LINK_TYPE.GAME
 );
